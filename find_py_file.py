336f86cb7a828225809539c96d721c120ae92fb2
SyDan0day <1293047033@qq.com>
2026-02-23 11:19:09 +0800

    test

--- find_py_file.py
+++ find_py_file.py
@@ -3,24 +3,20 @@
 ** 出指定路径下py文件的函数、类、空白行、代码行数量 V1.0 **
 ****************************************************
 """
-import os
+from pathlib import Path
 
-"""
-获取python文件的路径。
-参数:目标目录的路径str
-返回:目标目录下所有的python文件绝对路径str
-"""
 def get_py_path(dir_path) -> list:
-    file_path = []
-    for root, dirs, files in os.walk(dir_path):
-        for file in files:
-            if file.endswith('.py'):
-                file_path.append(os.path.join(root, file)) 
-    return file_path
+    """
+    获取python文件的路径。
+    参数:目标目录的路径(str)
+    返回:目标目录下所有的python文件绝对路径(str)
+    """
+    p = Path(dir_path)
+    return list(p.rglob('*.py'))
 
 def file_count(file_path) -> list:
 # 统计python文件的类、函数、空白行、代码行的数量。
-    with open(file_path, mode = 'r', encoding='UTF-8') as f:
+    with open(file_path, mode = 'r', encoding='utf-8') as f:
         lines = f.readlines()
         func_cnt = 0
         class_cnt = 0
@@ -48,4 +44,4 @@ def file_count(file_path) -> list:
 
 if __name__ == '__main__':
     dir_path = '/Users/bellchen/AI_Test'
-    print(file_count(get_py_path(dir_path)[0]))
+    print(get_py_path(dir_path))
